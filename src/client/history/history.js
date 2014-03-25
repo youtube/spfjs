fe363a443594f328950c105c6f6d13bb1ac29766
Andrew Braunstein <awbraunstein@google.com>
2014-03-25 09:42:00 -0800

    Log the entire window.history object in the pushState error message.
    [git-p4: change = 63741485]

--- src/client/history/history.js
+++ src/client/history/history.js
@@ -202,7 +202,22 @@ spf.history.doPushState_ = function(data, title, opt_url) {
   if (typeof window.history.pushState == 'function') {
     window.history.pushState(data, title, opt_url);
   } else {
-    throw new Error('history.pushState is not a function.');
+    // TODO(awbraunstein): Remove this once we gather information on
+    // the state of window.history at the time of the error.
+    var historyString = '';
+
+    for (var key in window.history) {
+      var value = window.history[key];
+      var valueString;
+      try {
+        valueString = JSON.stringify(value) || typeof value;
+      } catch (e) {
+        valueString = e.message;
+      }
+      historyString += (key + ': ' + valueString + '\n');
+    }
+    throw new Error('history.pushState is not a function. window.history: ' +
+        historyString);
   }
 };
 
